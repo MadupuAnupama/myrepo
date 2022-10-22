67053b7b32292dcae5815eb7f48bd2757f2e7ce8
Anupama Madupu <60696214+AnupamaMadupuTR@users.noreply.github.com>
2022-10-22 11:37:26 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/sh  
+#updating the os image
 sudo yum update –y
 
-#jenkins on AWS
+#installs jenkins on AWS
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
   https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
@@ -16,13 +17,13 @@ sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo systemctl enable docker
 
-#adding docker group to ec2user
+#adding docker group(permissions like run as admin) to ec2user
 sudo usermod -a -G docker ec2-user
 sudo usermod -a -G docker jenkins
 
 #installing git
 sudo yum install git -y
 
-#restarting the services
+#restarting the services as we add permissions
 sudo service jenkins restart
 sudo service docker restart
